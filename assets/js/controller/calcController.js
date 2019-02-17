a89300af33dba46ff7b17c292c77fd5aece6d1e0
Daniel Silva <dani3l.dxp@gmail.com>
2019-02-17 10:46:27 -0300

    Definindo data e hora local para o display da calculadora

--- assets/js/controller/calcController.js
+++ assets/js/controller/calcController.js
@@ -1,3 +1,64 @@
+//Classe 
 class CalcController {
-    
+    //Método construtor da classe
+    constructor(){
+        //Atributos da classe CalcController
+        this._locale = 'pt-BR'
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this._currentDate;
+        //Métodos da classe CalcController
+        this.initialize();
+    }
+
+    //Método principal
+    initialize(){
+        //Atualizando data e hora a cada segundo
+        setInterval(() => {
+            this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+            this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+        }, 1000)
+    }
+
+    //Método que retorna o valor da hora
+    get displayTime(){
+        return this._timeEl.innerHTML
+    }
+
+    //Método que atribui um novo valor a hora
+    set displayTime(time){
+        this._timeEl.innerHTML = time;
+    }
+
+    //Método que retorna o valor da data
+    get displayDate(){
+        return this._dateEl.innerHTML;
+    }
+
+    //Método que atribui um novo valor a data
+    set displayDate(date){
+        this._dateEl.innerHTML = date;
+    }
+
+    //Método que retorna o valor de _displayCalc
+    get displayCalc(){
+        return this._displayCalcEl.innerHTML;
+    }
+
+    //Método que atribui um novo valor ao _displayCalc
+    set displayCalc(value) {
+        this._displayCalcEl.innerHTML = valor;
+    }
+
+    //Método que retorna o valor de _currentDate
+    get currentDate(){
+        return new Date();
+    }
+
+    //Método que retorna o valor de _currentDate
+    set currentDate(date){
+        this._currentDate = data;
+    }
+
 }
